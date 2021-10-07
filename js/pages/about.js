f206f740dab78c65ab7b50e77ed2b4dfc4183563
dimitri <dimitri.bonnet18@gmail.com>
2021-10-07 20:34:15 +0200

    all

--- js/pages/about.js
+++ js/pages/about.js
@@ -30,66 +30,37 @@ class About {
     }
     openItem() {
         const items = document.querySelectorAll('.content__parcours .item')
-        const itemsHidden = document.querySelectorAll('.item__hidden')
         items.forEach((item) => {
             item.addEventListener('click', () => {
                 //Je vérifie si item a un enfant
                 if (item.children[1]) {
-                    if(item.children[1].classList.contains('itemIsActive')){
+                    if (item.children[1].classList.contains('itemIsActive')) {
+                        /* CONTENT */
                         item.children[1].classList.remove('itemIsActive')
+                        /* ICON */
+                        item.children[0].children[0].children[0].style.display = "block"
+                        item.children[0].children[0].children[1].style.display = "none"
                     } else {
+                        const itemsHidden = document.querySelectorAll('.item__hidden')
+                        const iconDown = document.querySelectorAll(".fa-chevron-circle-down")
+                        const iconUp = document.querySelectorAll(".fa-chevron-circle-up")
+                        /* CONTENT */
                         itemsHidden.forEach((i) => {
                             i.classList.remove('itemIsActive')
                         })
                         item.children[1].classList.add('itemIsActive')
+                        /* ICON */
+                        iconUp.forEach((i) => {
+                            i.style.display = "none"
+                        })
+                        iconDown.forEach((i) => {
+                            i.style.display = "block"
+                        })
+                        item.children[0].children[0].children[0].style.display = "none"
+                        item.children[0].children[0].children[1].style.display = "block"
                     }
                 }
             })
         })
     }
 }
-
-
-
-
-/* const scene = new THREE.Scene();
-scene.add(new THREE.AxesHelper())
-console.log(scene);
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000)
-camera.position.z = 2;
-camera.position.y = 0.5
-camera.position.x = 0.5
-scene.add(camera)
-
-const cube = new THREE.Mesh(
-    new THREE.BoxBufferGeometry(1,1,1),
-    new THREE.MeshNormalMaterial()
-)
-scene.add(cube)
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-document.body.appendChild(renderer.domElement)
-renderer.render(scene, camera)
-
-
-const controls = new THREE.OrbitControls();
-
-
-
-function tick () {
-    renderer.render(scene, camera);
-    camera.position.x += 0.01
-    controls.update()
-    camera.lookAt(0,0,0)
-    requestAnimationFrame(tick)
-}
-
-tick()
-
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-}) */
